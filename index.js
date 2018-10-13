1703a9a5ce57dd015e24ca06ef9c43f72abcb291
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-10-13 12:32:10 +0200

    Added sequelize

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-const { Client } = require('pg');
-
-const connectionString = 'postgresql://postgres:secret@localhost:5432/homework';
-const client = new Client({ connectionString });
-
-client.connect();
-
-app.get('/do-something', (request, response) => {
-  console.log(`I'll do something, I promise!`);
-  response.end();
+const Sequelize = require('sequelize');
+const connectionString =
+  process.env.DATABASE_URL ||
+  'postgres://postgres:secret@localhost:5432/homework';
+const sequelize = new Sequelize(connectionString, {
+  define: { timestamps: false }
 });
 
-app.get('/houses/:id', (request, response) => {
-  const houseId = request.params.id;
-
-  client.query(
-    'SELECT * FROM houses WHERE id = $1',
-    [houseId],
-    (error, result) => {
-      if (error) {
-        // HTTP 500 = Internal Server Error
-        response.status(500).send({
-          message: 'Something went wrong with Postgres!',
-          details: error.message
-        });
-      } else {
-        response.send(result.rows[0]);
-      }
+const Playlist = sequelize.define(
+  'playlists',
+  {
+    name: {
+      type: Sequelize.STRING,
+      field: 'name',
+      allowNull: false
     }
-  );
+  },
+  {
+    timestamps: false,
+    tableName: 'playlists'
+  }
+);
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log('Sequelize updated database schema');
+  })
+  .catch(console.error);
+
+const port = process.env.PORT || 4003;
+
+app.get('/', (request, response) => {
+  console.log(`I'll do something, I promise!`);
+  response.send('there is something');
 });
 
-app.listen(4001, () => console.log('Express API listening on port 4001'));
+app.get('/playlists', (req, res, next) => {
+  Playlist.findAll().then(playlist => res.send(playlist));
+});
+
+app.listen(port, () => console.log(`Express API listening on port ${port}`));
